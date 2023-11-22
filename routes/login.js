76d3fcc26f3daf6e9cb1a5df5b4b851c4ba60e6b
aswinshub <aswin1998ply@gmail.com>
2023-11-22 11:17:18 +0530

    auth

--- routes/login.js
+++ routes/login.js
@@ -7,39 +7,28 @@ const empData = require("../model/empSchema");
 require("../db/connect");
 
 
-// POST Method----------
-
-// router.post('/', async (req, res) => {
-//     try {
-//         const { name, password } = req.body;
-
-//         const foundUser = await empData.findOne({ name, password });
-
-//         if (foundUser || (email === 'admin' && password === 'admin')) {
-//             res.status(200).send('Login successful');
-//         } else {
-//             res.status(200).send('Invalid credentials');
-//         }
-//     } catch (error) {
-//         console.error('Error during login:', error);
-//         res.status(500).send(error);
-//     }
-// });
-
 router.post('/', async (req, res) => {
     try {
-        const { name, password } = req.body;
+        const { email, password } = req.body;
+        let token = null;
 
         // Check if the user is in the database
-        const foundUser = await empData.findOne({ name, password });
+        const foundUser = await empData.findOne({ email, password });
 
-        if (foundUser || (name === 'admin' && password === 'admin')) {
-            // Database user found or admin credentials matched
-            res.status(200).send({ message: 'success' });
+        if (foundUser) {
+            // Database user found, use a different secret
+            let payload = { email: email, password: password };
+            token = jwt.sign(payload, 'reactempapp');
+        } else if (email === 'admin' && password === 'admin') {
+            // Check hardcoded admin credentials
+            let payload = { email: email, password: password };
+            token = jwt.sign(payload, 'reactempapp');
         } else {
             // No matching credentials
-            res.status(401).send('Invalid credentials');
+            return res.status(401).send('Invalid credentials');
         }
+
+        res.status(200).send({ message: 'success', token: token });
     } catch (error) {
         console.error('Error during login:', error);
         res.status(500).send(error);
